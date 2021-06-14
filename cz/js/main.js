9ce4e088a2040d948f40a857272aa6f9c1c57fdc
Khoroshavin <khoroshavin.russia@gmail.com>
2021-06-14 17:49:15 +0200

    ready 2 versions with fixs

---
+++ cz/js/main.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () =>{
+  
+  const slideModal = document.querySelector('.slide__modal');
+  const closeBtn = document.querySelector('.slide__btn_close');
+  const slideModalLeft = document.querySelector('.left');
+
+  function openSlideModal() {
+    slideModal.classList.toggle("slide__modal_open");
+  }
+
+  function closeSlideModal() {
+    closeBtn.onclick = function() {
+      slideModal.classList.toggle("slide__modal_open");
+    };
+  }
+
+  function openSlideModalOnClick() {
+    slideModalLeft.onclick = function () {
+      slideModal.classList.toggle("slide__modal_open");
+    };
+  }
+  
+  setTimeout(openSlideModal, 5000);
+  closeSlideModal();
+  openSlideModalOnClick();
+}, false);
